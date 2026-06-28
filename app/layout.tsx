7505093ad26be3b43455d4bcb084379fc1ce1acb
juank2140 <aura.comerce@gmail.com>
2026-06-28 09:08:30 -0500

    Add Meta Pixel 2020636644865927

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,16 @@ export default function RootLayout({
     >
       <head>
         <Script src="https://checkout.bold.co/library/boldPaymentButton.js" strategy="beforeInteractive" />
+        <Script id="meta-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+          n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+          document,'script','https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init','2020636644865927');
+          fbq('track','PageView');
+        `}</Script>
+        <noscript><img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=2020636644865927&ev=PageView&noscript=1" /></noscript>
       </head>
       <body className="font-sans antialiased">
         {children}
